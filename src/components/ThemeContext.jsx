cbe95d7aeaa72606be54b2d6d509626a46534747
Vedika_Sharma <155888866+VedikaGeek@users.noreply.github.com>
2024-12-04 21:52:56 +0530

    Update ThemeContext.jsx

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -1,17 +1,19 @@
 import React, { createContext, useState } from 'react';
 
+
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState(false); // Ensure this is a boolean
+    const [theme, setTheme] = useState(false); 
 
     const toggleTheme = () => {
         setTheme(prevTheme => !prevTheme);
+        
     };
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            {children}
+            {children && children.toggle}
         </ThemeContext.Provider>
     );
 };
